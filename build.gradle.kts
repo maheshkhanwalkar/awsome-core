a9e1a5f4bded2e5be5b6f20e3161cd3aaf76fec4
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2021-08-15 09:11:47 -0400

    build: setup gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    java
+    kotlin("jvm") version "1.5.21"
+}
+
+group = "com.revtekk"
+version = "0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "awsome-core"
+
